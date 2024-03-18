e58627c80d9306cf9d222e247d0412900b8345d4
Davi Nunes <davi.nunes@gmail.com>
2024-03-17 23:36:39 -0300

    Update Torre_C.ino

--- codigos/Torre_C/Torre_C.ino
+++ codigos/Torre_C/Torre_C.ino
@@ -18,9 +18,6 @@
  
 int idSensor = 5; 
 
-// Qual feed receberá as leituras na adafruit?
-#define FEED_PERC   "/feeds/Torre_C_P"
-
 // A que distancia a sonda está do nivel máximo de água?
 int distanciaSonda = 20; 
 // Qual a altura maxima da coluna de água? (Não considerar a distancia da Sonda)
@@ -36,7 +33,7 @@ int alturaAgua = 240;
  * Torre F Reservatório 01
  * */
  
-String nomeDaSonda = "Torre C Reservatório 01";
+String nomeDaSonda = "Torre D Reservatório 01";
 
 // Qual Nome da Rede Wifi na casa de máquinas?
 
@@ -59,17 +56,7 @@ String IntervaloDePush      = "https://raw.githubusercontent.com/davinunes/TopLi
 String nivelAlertaTelegram  = "https://raw.githubusercontent.com/davinunes/TopLifeMiami-Nivel-de-gua/main/parametros/nivelAlerta";
 String novoUrlSite          = "https://raw.githubusercontent.com/davinunes/TopLifeMiami-Nivel-de-gua/main/parametros/novoUrlSite";
 
-
-//Variaveis para usar para enviar mensagem no telegram
-String chaveTelegram = "5199663658:AAF4D8-KtthX87TGX6pYHBiLGTTZYPyU3Z8";
-String chat = "-1001158157448"; //Chat do telgram que receberá qualquer leitura do sensor
-String alert = "-1001601389998"; // Chat do telgram que receberá mensagem apenas quando o nivel da água estiver abaixo do esperado
 String urlSite = "h2o-miami.davinunes.eti.br";
-
-// Variaveis relacionadas a Adafruit
-#define IO_USERNAME "ilunne" //usuario
-#define IO_KEY2     "hMjk89XNaWhSBc7UxR70upfJch2A" //Cole apenas a parte depois do underline
-
 
 /* INICIALIZA O Wifi
    e o cliente http
@@ -98,40 +85,13 @@ String StatusInternet = "Sem Wifi...";
 #include "SSD1306Wire.h"
 SSD1306Wire display(0x3c, 21, 22);
 
-
-/* Biblioteca Adafruit */
-
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
-#define IO_SERVER     "io.adafruit.com"
-#define IO_SERVERPORT 1883
-#define IO_KEY1    "aio_"
-#define IO_USERNAME "ilunne"
-#define IO_KEY2    "hMjk89XNaWhSBc7UxR70upfJch2A"
-
-Adafruit_MQTT_Client mqtt(&client, IO_SERVER, IO_SERVERPORT, IO_USERNAME, IO_KEY1 IO_KEY2);
-//Adafruit_MQTT_Publish sonar01 = Adafruit_MQTT_Publish(&mqtt, IO_USERNAME FEED_CM, MQTT_QOS_1);
-Adafruit_MQTT_Publish sonar02 = Adafruit_MQTT_Publish(&mqtt, IO_USERNAME FEED_PERC, MQTT_QOS_1);
-Adafruit_MQTT_Subscribe wifiTime = Adafruit_MQTT_Subscribe(&mqtt, IO_USERNAME "/feeds/PlacaDSV_update_time");
-
-
-
 /* INICIALIZA O Sonar com a LIB Ultrasonic.h
    Modelos HC-SR04 ou SEN136B5B
 */
 
-#define TRIGGER_PIN  33 // Arduino pin tied to trigger pin on ping sensor.
-#define ECHO_PIN1    25 // Arduino pin tied to echo pin on ping sensor.
+#define TRIG_PIN  33 // Arduino pin tied to trigger pin on ping sensor.
+#define ECHO_PIN  25 // Arduino pin tied to echo pin on ping sensor.
 
-
-#include <Ultrasonic.h>
-
-Ultrasonic sonar1(TRIGGER_PIN, ECHO_PIN1, 40000UL);
-
-int distancex;          // Distancia medida pelo sensor em CM
-int distance;         // Distancia medida pelo sensor em CM
-int minimo;            // Menor distancia já medida
-int maximo;            // Maior distancia já medida
 int progresso = 0;      // Calculo da % da barra de progresso
 
 
@@ -153,14 +113,9 @@ int progresso = 0;      // Calculo da % da barra de progresso
  */
 
 String wget (String url);
-void telegramLog(String mensagem);
-void telegramAlarm(String mensagem);
 void eti(int num);
-void conectar_broker();
-void callback();
 void sonar();
 void tela();
-void IoT();
 void internet();
 void getParametrosRemotos();
 
@@ -168,17 +123,32 @@ void getParametrosRemotos();
  * Setup
  */
 
+int distance;         // Distancia medida pelo sensor em CM
+int minimo;            // Menor distancia já medida
+int maximo;            // Maior distancia já medida
+
 void setup() {
   Serial.begin(115200);
   internet();
-
+   
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+   
   // INICIALIZA O DISPLAY & INVERTE O DISPLAY VERTICALMENTE
   display.init();
   display.flipScreenVertically();
+   
+   WiFi.begin(ssid, password);
+   int i = 0;
+       while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      if (i++ > 10) {
+        break;
+      }
 
+    }
 
-  // Realiza as inscrições MQTT
-  mqtt.subscribe(&wifiTime);
 }
 
 //========================================================================
@@ -216,21 +186,6 @@ String wget (String url) {
   http.end();
 }
 
-void telegramLog(String mensagem) {
-  // ColeSeuTokenAqui ColeIDdoGrupoAqui TestandoEnvio
-  // https://api.telegram.org/bot5199663658:AAF4D8-KtthX87TGX6pYHBiLGTTZYPyU3Z8/getUpdates
-  String url = "https://api.telegram.org/bot" + chaveTelegram + "/sendMessage?chat_id=" + chat + "&text=" + mensagem;
-  wget(url);
-}
-void telegramAlarm(String mensagem) {
-  if ((millis() - lastTimeAlert) > timerAlerta) {
-  // ColeSeuTokenAqui ColeIDdoGrupoAqui TestandoEnvio
-  String url = "https://api.telegram.org/bot" + chaveTelegram + "/sendMessage?chat_id=" + alert + "&text=" + mensagem;
-  wget(url);
-  lastTimeAlert = millis(); // Esta linha sempre no final BLOCO QUE CONTROLA O TIMER!
-  }
-}
-
 void eti(int num) {
   // ColeSeuTokenAqui ColeIDdoGrupoAqui TestandoEnvio
   //Vamos obter a URL perguntando também ao github
@@ -238,55 +193,23 @@ void eti(int num) {
   Serial.println(wget(url));
 }
 
-/* Conexão com o broker e também servirá para reestabelecer a conexão caso caia */
-void conectar_broker() {
-  int8_t ret;
-
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.println("Conectando-se ao broker mqtt...");
-
-  uint8_t num_tentativas = 3;
-  while ((ret = mqtt.connect()) != 0) {
-    Serial.println(mqtt.connectErrorString(ret));
-    Serial.println("Falha ao se conectar. Tentando se reconectar em 5 segundos.");
-    mqtt.disconnect();
-    delay(5000);
-    num_tentativas--;
-    if (num_tentativas == 0) {
-      Serial.println("Provavelmente não tem internet.");
-      break;
-    }
-  }
-
-  Serial.println("Conectado ao broker com sucesso.");
-}
-
-void callback() {
-  Adafruit_MQTT_Subscribe *subscription;
-  while ((subscription = mqtt.readSubscription(5000))) {
-    if (subscription == &wifiTime) {
-      Serial.print(F("Got: "));
-      Serial.println((char *)wifiTime.lastread);
-    }
-  }
-}
-
 void sonar() {
-  distance = sonar1.read();
-  distance = distance - distanciaSonda;
-  Serial.print("Sonar -> ");
-  Serial.println(distance);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(20);
+  digitalWrite(TRIG_PIN, LOW);
 
-  if (minimo == 0 || minimo > distance) {
-    minimo = distance;
+  const unsigned long duration= pulseIn(ECHO_PIN, HIGH);
+  distance= duration/29/2;
+  if(duration==0){
+    Serial.println("Warning: no pulse from sensor");
+  } else {
+    Serial.print("distance to nearest object:");
+    Serial.println(distance);
+    Serial.println(" cm");
   }
-
-  if (maximo == 0 || maximo < distance) {
-    maximo = distance;
-  }
+  delay(100);
 
   distance > alturaAgua ? progresso = alturaAgua : progresso = distance;
   progresso = 100 * progresso / alturaAgua;
@@ -317,28 +240,12 @@ void IoT() {
   if ((millis() - lastTime) > timerDelay) {
     if (WiFi.status() == WL_CONNECTED) {
       Serial.println("HORA DE TAREFAS DA WEB");
-
-      // Lê o que tiver de novo na Adafruit
-      callback();
       
       String msg = nomeDaSonda + " -> Distancia do Sensor: " + String(distance) + "cm";
-      telegramLog(msg);
+      
       eti(distance);
 
-      conectar_broker();
       getParametrosRemotos();
-
-      if (!sonar02.publish(progresso)){
-        Serial.println("Erro ao publicar na Adafruit");
-      } else {
-        Serial.println("Publicado na Adafruit");
-      }
-
-      if (progresso < nivelAlerta) {
-        String msg = nomeDaSonda + " -> NIVEL DE ÁGUA ABAIXO DO ESPERADO! => Reservatório está em " + String(progresso) + "%";
-        telegramAlarm(msg);
-      }
-      //Serial.println(String(sonar01.publish(distance)));
       lastTime = millis(); // Esta linha sempre no final BLOCO QUE CONTROLA O TIMER!
 
     }
@@ -351,19 +258,11 @@ void internet(){
     return;
   } else {
     StatusInternet = "Sem Wifi...";
-    WiFi.begin(ssid, password);
-    Serial.println("Conectando na rede WiFi");
-    int i = 0;
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-      if (i++ > 10) {
-        break;
-      }
-
-    }
-    Serial.println("Conectado com IP: ");
-    Serial.println(WiFi.localIP());
+    
+    //Serial.println("Conectando na rede WiFi");
+    
+    //Serial.println("Conectado com IP: ");
+    //Serial.println(WiFi.localIP());
   }
 }
 
